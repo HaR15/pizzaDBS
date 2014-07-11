1ff77c403bdea04754a86ddc40342251e5310e37
Haris Shoaib <haris005@gmail.com>
2014-07-11 16:52:47 -0400

    Updated files

--- login.java
+++ login.java
@@ -6,13 +6,52 @@ public class login {
 	private String password;
 	
 	public login(){
+		// Setup scanner
 		Scanner input = new Scanner(System.in);
+		String fillers = "================================";
+		
+		promptUsernamePassword(input);
+		//Check if username and password exists in db
+		while(!userExists()){
+			System.out.println("Incorrect username or password, try again.");
+			promptUsernamePassword(input);
+		}
+		// User exists
+		System.out.println(fillers);
+		new customer(this.username, this.password);
+	}
+	
+	/* Prompts user for username, password and sets the
+	 * inputed Strings to this.username and this.password
+	 */
+	public void promptUsernamePassword(Scanner input){
 		System.out.println("Input your username: ");
-		this.username = input.nextLine();
+		this.username = input.nextLine(); 
+		this.username = isEmpty(this.username, input);
 		System.out.println("Input your password: ");
 		this.password = input.nextLine();
-		//Check if username and password exists in db
-
+		this.password = isEmpty(this.password, input);
+	}
+	
+	/* Checks if input fields are empty, and requests
+	 * re-input if they are. Returns input when given.  
+	 */ 
+	public String isEmpty(String userInput, Scanner input){
+		while(userInput.isEmpty()){
+			System.out.println("Try again.");
+			userInput = input.nextLine();
+		}
+		return userInput;
+	}
+	
+	/*
+	 * Returns true if the user with the inputed username 
+	 * and password matches any tuple in the customers table.
+	 */
+	public Boolean userExists(){
+		/* * GOTTA DO THIS * */
+		// query database to see if username = this.username and password = this.password 
+		return true;
 	}
 
 }
--- register.java
+++ register.java
@@ -8,7 +8,9 @@ public class register {
 	private String sex; 
 	
 	public register(){
+		// Setup scanner for inputs
 		Scanner input = new Scanner(System.in);
+		String fillers = "================================";
 		
 		System.out.println("Please enter a username: ");
 		this.username = input.nextLine();
@@ -25,13 +27,23 @@ public class register {
 		System.out.println("Enter your sex (M/F)");
 		this.sex = input.nextLine().toUpperCase();
 		this.sex = isEmpty(this.sex, input);
+		// Keeps prompting user for re-entry to get correct value 
+		// of their sex
 		while(!(this.sex.equals("M")) && !(this.sex.equals("F"))){
 			System.out.println("Incorrect input. Try again!");
 			this.sex = input.nextLine().toUpperCase();
-		}
-		
-		
+		}	
+		System.out.println("\nThank you for Registering");
+		System.out.println("Taking you back now to the home menu");
+		System.out.println(fillers);
+		//Takes back to Home menu (in main() of main.java)
+		main.main(null);
 	}
+	
+	/*
+	 * Keeps prompting user for re-entry to get a value 
+	 * of user's sex. Returns the correct input. 
+	 */
 	public String isEmpty(String userInput, Scanner input){
 		while(userInput.isEmpty()){
 			System.out.println("Try again.");
