14f9de7ef2447edfcec2b058cabcc50c8febc28b
Haris Shoaib <haris005@gmail.com>
2014-07-23 23:10:01 -0400

    Many changes to pizza.java and added viewCart.java

---
+++ piaaz/src/viewcart.java
@@ -0,0 +1,23 @@
+
+public class viewcart {
+
+	private String username = null;
+	private String password = null;
+	
+	public viewcart(String username, String password){
+		this.username = username;
+		this.password = password;
+	}
+	public void addPizzas(){
+		
+	}
+	
+	public void addSides(){
+		
+	}
+	
+	public void display(){
+		
+	}
+
+}
